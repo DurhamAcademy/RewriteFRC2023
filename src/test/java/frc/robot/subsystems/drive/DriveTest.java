213ce3656227406b6172badc3fece877bfd6edb6
a1cd <71281043+a1cd@users.noreply.github.com>
2023-08-13 11:19:16 -0400

    added test

---
+++ src/test/java/frc/robot/subsystems/drive/DriveTest.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.drive;
+
+import org.junit.jupiter.api.Test;
+
+class DriveTest {
+
+    @Test
+    void periodic() {
+    }
+
+    @Test
+    void drivePercent() {
+    }
+
+    @Test
+    void driveArcade() {
+    }
+
+    @Test
+    void stop() {
+    }
+
+    @Test
+    void getPose() {
+    }
+
+    @Test
+    void getLeftPositionMeters() {
+    }
+
+    @Test
+    void getRightPositionMeters() {
+    }
+
+    @Test
+    void getLeftVelocityMeters() {
+    }
+
+    @Test
+    void getRightVelocityMeters() {
+    }
+}
